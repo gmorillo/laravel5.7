0c83731aa9e1dc6c56e5ad8c57cc04a413d537c1
Guillermo Morillo <gmorillo@nscontrol.es>
2018-12-04 18:30:54 +0100

    vistas de perfil

--- resources/views/sections/profile/botones-comprar/botones.blade.php
+++ resources/views/sections/profile/botones-comprar/botones.blade.php
@@ -2,23 +2,23 @@
 	<div class="col bg-white p-3 mx-1">
 		<h3 class="text-center py-2"><strong>Rotador Principal</strong></h3>
 		<ul class="py-3 text-center">
-			<li class="py-3">Publica tu anuncio en la sección mas grande de nuestra web.</li>
-			<li class="py-3">Elige los días y las horas en las que quieres que aparezca tu anuncio.</li>
-			<li class="py-3">Date a conocer en Panamá.</li>
-			<li class="py-3">Sólo pagarás por el tiempo que este activo tu anuncio.</li>
-			<li class="py-3">Lorem ipsum dolor sit amet.</li>
-			<li class="py-3">Lorem ipsum dolor sit.</li>
+			<li class="py-3">Publica tu anuncio en la <strong>sección mas grande</strong> de nuestra web.</li>
+			<li class="py-3"><strong>Elige los días y las horas</strong> en las que quieres que aparezca tu anuncio.</li>
+			<li class="py-3">Date a conocer en <strong>Ciudad.</strong></li>
+			<li class="py-3">Agrega hasta 10 fotos en tu anuncio</li>
+			<li class="py-3">Tu anuncio se actualiza <strong>cada hora para aparecer en las primeras posiciones.</strong></li>
+			<li class="py-3">Aumenta visitas en un <strong>100%</strong></li>
 		</ul>
 	</div>
 	<div class="col bg-white p-3 mx-1">
 		<h3 class="text-center py-2"><strong>Plan Premium</strong></h3>
 		<ul class="py-3 text-center">
-			<li class="py-3">Lorem ipsum dolor sit amet, consectetur.</li>
-			<li class="py-3">Lorem ipsum dolor sit.</li>
-			<li class="py-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-			<li class="py-3">Lorem ipsum dolor.</li>
-			<li class="py-3">Lorem ipsum dolor sit amet.</li>
-			<li class="py-3">Lorem ipsum dolor sit.</li>
+			<li class="py-3">Publica tu anuncio en la <strong>sección premium</strong> de nuestra web.</li>
+			<li class="py-3"><strong>Elige los días y las horas</strong> en las que quieres que aparezca tu anuncio.</li>
+			<li class="py-3">Tienes el privilegio de aparecer entre las <strong>primeras publicaciones cada minuto.</strong></li>
+			<li class="py-3">Agrega hasta 5 fotos en tu anuncio</li>
+			<li class="py-3">Sólo pagarás por el tiempo que este activo tu anuncio.</li>
+			<li class="py-3">Aumenta visitas en un <strong>90%</strong></li>
 		</ul>
 	</div>
 	<div class="col bg-white p-3 mx-1">
