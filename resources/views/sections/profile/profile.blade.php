241b259943c1620860cda06e0c1a3c296ba780ad
Guillermo Morillo <gmorillo@nscontrol.es>
2018-12-05 18:27:58 +0100

    modificaciones de estilos

--- resources/views/sections/profile/profile.blade.php
+++ resources/views/sections/profile/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container p-0">
+<div class="container">
     <div class="row ">
     	<div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-xs-12 mt-4">
 			@include('sections.profile.menu-izquierdo-profile')
