2d276eb71ffc1b8cf303ce01b3ef0eb20517509c
Guillermo Morillo <gmorillo@nscontrol.es>
2019-01-18 13:46:11 +0100

    afinando detalles

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,16 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid bg-danger p-0 py-4">
+    <h3 class="text-center text-white">Cambio de contraseña</h3>
+</div>
+<div class="container my-5">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header bg-info text-white">{{ __('Cambiar contraseña') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                            {{-- session('status') --}}
+                            ¡Hemos enviado por correo electrónico el enlace para restablecer su contraseña!
                         </div>
                     @endif
 
@@ -18,14 +22,15 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Correo Electrónico') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
+                                        {{--<strong>{{ $errors->first('email') }}</strong>--}}
+                                        <strong>No podemos encontrar un usuario con esa dirección de correo electrónico.</strong>
                                     </span>
                                 @endif
                             </div>
@@ -34,7 +39,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('Enviar enlace para Restablecer contraseña') }}
                                 </button>
                             </div>
                         </div>
