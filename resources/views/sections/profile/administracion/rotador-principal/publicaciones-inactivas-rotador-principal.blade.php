4a4661f8866f072cad4e13bf0f2dcdd34437ade3
Guillermo Morillo <morillo_guillermo@hotmail.com>
2019-01-19 15:55:59 +0100

    eliminar publicación e imágenes

--- resources/views/sections/profile/administracion/rotador-principal/publicaciones-inactivas-rotador-principal.blade.php
+++ resources/views/sections/profile/administracion/rotador-principal/publicaciones-inactivas-rotador-principal.blade.php
@@ -61,29 +61,9 @@
 					<td scope="row">{{$slider->creation_date}}</td>
 					<td scope="row">
 						<a href="{{ url('profile/administracion/editar-rotador-principal/') }}/{{$slider->id}}" title="Editar anuncio"><i class="fas fa-edit"></i></a>
-						<a href="{{ url('profile/administracion/eliminar-publicidad/') }}/{{$slider->id}}" style="cursor: pointer;" data-toggle="modal" data-target="#pubinactivas"  title="Eliminar anuncio"><i class="fas fa-trash-alt"></i></a>
+						<a href="{{ url('profile/administracion/eliminar-publicidad/') }}/{{$slider->id}}" style="cursor: pointer;"  title="Eliminar anuncio"><i class="fas fa-trash-alt"></i></a>
 					</td>
 				</tr>
-				<!-- Modal -->
-				<div class="modal fade" id="pubinactivas" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				  <div class="modal-dialog" role="document">
-				    <div class="modal-content">
-				      <div class="modal-header">
-				        
-				        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-				          <span aria-hidden="true">&times;</span>
-				        </button>
-				      </div>
-				      <div class="modal-body">
-				        <h5 class="modal-title" id="exampleModalLabel">¿Desea eliminar este anuncio?</h5>
-				      </div>
-				      <div class="modal-footer">
-				        <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">Cerrar</button>
-				        <a class="btn btn-outline-success" href="{{ url('profile/administracion/eliminar-publicidad/') }}/{{$slider->id}}"  title="Eliminar anuncio"><i class="fas fa-trash-alt"></i> eliminar</a>
-				      </div>
-				    </div>
-				  </div>
-				</div>
 			@endforeach
 		</tbody>
 	</table>
